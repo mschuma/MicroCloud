6eeaca3324621aaa3f31851a7ad3d2ca4320bd0a
Devendra D. Chavan <ddchavan@ncsu.edu>
2013-03-03 14:32:30 -0500

    Get the git user.name and git user.email as arguments. Update source repo URL

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [[($1 = "") || ($2 = "")]]; then
+    echo "Usage: dev_setip.sh <git user.name> <git user.email>"
+    exit 1
+fi
+
 #Install git
 cd $HOME
 wget http://git-core.googlecode.com/files/git-1.8.1.4.tar.gz 
@@ -9,15 +14,14 @@ cd git-1.8.1.4
 make 
 make install 
 export PATH=$PATH:$HOME/git/bin
-git config --global user.name="Devendra D. Chavan"
-git config --global user.email="ddchavan@ncsu.edu"
+git config --global user.name=$1
+git config --global user.email=$2
 ssh-keygen
-echo "Copy this key to bitbucket/github. Hit enter after copying"
+echo "Copy this key to github. Hit enter after copying"
 echo
 cat $HOME/.ssh/id_rsa.pub
 echo
 read
 cd $HOmE
-mkdir installer
-git clone git@bitbucket.org:ddchavan/microcloud-installer.git
+git clone git@github.com:mschuma/MicroCloud.git
 git status
