1697a413d1445208dfa30263780058e1b3b1aecc
Devendra D. Chavan <ddchavan@ncsu.edu>
2013-03-04 00:21:33 -0500

    Download and extract perl modules

--- installer/php/install.py
+++ installer/php/install.py
@@ -6,67 +6,52 @@ import tarfile
 import os
 from subprocess import call
 from os.path import expanduser
+from tempfile import mkstemp
+from shutil import move
+import shutil
+from os import remove, close
 
 def read_file(filepath):
-    f = open(filepath)
-    lines = f.readlines()
-    f.close
+    dictionary = dict()
+    for line in open(filepath):
+        key, value = line.split()
+        dictionary[key.strip()] = value.strip() 
+    
+    return dictionary
 
-    return lines
-
-def download_modules(readlines):
-    for line in readlines:
-        (module_name, url) = line.split( )
-        #TODO: Check for errors
+def download_modules(modules_dict):
+    print "Downloading modules"
+    for (module_name, url) in modules_dict.iteritems():
+        # TODO: Check for errors
         filename = url.split('/')[-1]
-        #TODO: Include checksum in config file to verify
+        # TODO: Include checksum in config file to verify
+        # TODO: Pretty print in tabular format
+        print "[%s] from %s ..." % (module_name, url)
         urllib.urlretrieve(url, filename)
 
-def extract_modules(readlines):
-    home = expanduser("~")
-    libpath = home+"/lib"
-    makefile_prefix="PREFIX="+libpath
-    makefile_libpath ="LIB="+libpath
-    saved_path = os.getcwd()
-    
-    for line in readlines:
-        (module_name, url) = line.split( )
+    print "Completed downloading modules\n"
+
+def extract_modules(modules_dict):
+    print "Extracting modules"
+    for (module_name, url) in modules_dict.iteritems():
+        # TODO: Can use key/value pair in the config file
         # TODO: Check if the file exists 
         filename = url.split('/')[-1]
+        print "[%s] from %s ..." % (module_name, filename)
         tar = tarfile.open(filename)
         tar.extractall()
         tar.close()
         folder_name = filename.strip(".tar.gz")
 
-        os.chdir(folder_name)
-        call(["perl", "Makefile.PL", makefile_prefix, makefile_libpath])
-        os.chdir(saved_path)
-
-    os.chdir(saved_path)
-
-def make_install_modules(readlines):
-    saved_path = os.getcwd()
-    
-    for line in readlines:
-        (module_name, url) = line.split( )
-        # TODO: Check if the file exists 
-        filename = url.split('/')[-1]
-        folder_name = filename.strip(".tar.gz")
-
-        os.chdir(folder_name)
-        call("make")
-        call(["make", "install"])
-        os.chdir(saved_path)
-
-    os.chdir(saved_path)
+    print "Completed extracting modules\n"
 
 def install_modules(config_filepath):
+    # TODO: Move common methods to utility library
     # TODO: Add unit tests
-    readlines=read_file(config_filepath)
-    download_modules(readlines)
-    extract_modules(readlines)
-    make_install_modules(readlines)
+    modules_dict = read_file(config_filepath)
+    download_modules(modules_dict)
+    extract_modules(modules_dict)
 
-#install_modules("pre_requisites")
-#install_modules("main")
+install_modules("pre_requisites")
+install_modules("main")
 install_modules("post_main")
