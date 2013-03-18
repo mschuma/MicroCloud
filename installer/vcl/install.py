18bb911af92a92fbd6e5000ce892a94959d3fce5
Devendra D. Chavan <ddchavan@ncsu.edu>
2013-03-17 22:41:17 -0400

    Add logic for installing the VCL front-end code

--- installer/vcl/install.py
+++ installer/vcl/install.py
@@ -13,6 +13,8 @@ from os import remove, close
 import md5
 import MySQLdb as db
 import sys
+import random
+import string
 
 def read_file(filepath):
     dictionary = dict()
@@ -73,6 +75,10 @@ def get_file_name(modules_dict, module_name):
     filename = modules_dict[module_name].split('/')[-1]
     return filename
 
+def generate_random_text():
+   random_chars = "".join([random.choice(string.letters) for i in xrange(30)])
+   return random_chars
+
 def install_modules(config_filepath):
     # TODO: Add unit tests
     modules_dict = read_file(config_filepath)
@@ -88,9 +94,90 @@ def install_modules(config_filepath):
     extract_modules(modules_dict)
     
     vcl_path = get_folder_path(modules_dict, "vcl")
-    install_mysql(vcl_path)
+    #install_vcl(vcl_path)
+    # TODO: Retreive the public IP address of the current machine (VM or 
+    #       otherwise)
+    install_webserver("vcl","localhost","vcluser", "vcluserpassword", 
+                        "152.7.98.119")
 
-def install_mysql(vcl_path):
+def install_webserver(vcl_db, vcl_host, vcl_username, vcl_password, 
+                        webserver_ip_address):
+    # TODO: Modules list can be moved to config file
+    call(["yum", "install", "httpd", "mod_ssl", "php", "php-gd", "php-mysql", 
+            "php-xml", "php-xmlrpc", "php-ldap", "-y"])
+    call(["/sbin/chkconfig", "--level", "345", "httpd", "on"])
+    call(["/sbin/service", "httpd", "start"])
+
+    # Install the front end
+    saved_path = os.getcwd()
+    shutil.copytree("apache-VCL-2.3.1/web/", "/var/www/html/vcl")
+    os.chdir("/var/www/html/vcl/.ht-inc")
+    shutil.copy("secrets-default.php", "secrets.php")
+
+    # Update the default values
+    fh, abs_path = mkstemp()
+
+    old_file = open("secrets.php", 'r')
+    temp_file = open(abs_path, 'w')
+
+    dictionary = dict()
+    dictionary["$vclhost"] = "'%s'" % vcl_host
+    dictionary["$vcldb"] = "'%s'" % vcl_db
+    dictionary["$vclusername"] = "'%s'" % vcl_username
+    dictionary["$vclpassword"] = "'%s'" % vcl_password
+    dictionary["$cryptkey"] = "'%s'" % generate_random_text()
+    dictionary["$pemkey"] = "'%s'" % generate_random_text()
+
+    for line in old_file:
+        if "=" in line:
+            key, value = line.split('=')
+            newline = "%s = %s;%s" % (key.strip(), 
+                                        dictionary[key.strip()], 
+                                        value.split(';')[1])
+            temp_file.write(newline)
+        else:
+            temp_file.write(line)
+    
+    old_file.close()
+    temp_file.close()
+    close(fh)
+
+    remove("secrets.php")
+    move(abs_path, "secrets.php")
+
+    call("./genkeys.sh")
+    shutil.copy("conf-default.php", "conf.php")
+    
+    # Update the IP address in conf.php
+    fh, abs_path = mkstemp()
+
+    old_file = open("conf.php", 'r')
+    temp_file = open(abs_path, 'w')
+
+    for line in old_file:
+        if "vcl.example.org" in line:            
+            newline = line.replace("vcl.example.org", webserver_ip_address)
+            temp_file.write(newline)
+        elif "\".example.org\"" in line:
+            newline = line.replace(".example.org", "")
+            temp_file.write(newline)
+        else:
+            temp_file.write(line)
+    
+    old_file.close()
+    temp_file.close()
+    close(fh)
+
+    remove("conf.php")
+    move(abs_path, "conf.php")
+    
+    # TODO: Update the help and error email ids in conf.php
+
+    call(["chown", "apache", "maintenance"])
+    
+    os.chdir(saved_path)
+
+def install_vcl(vcl_path):
     # Install mysql
     # TODO: Check if mysql is installed before installing
     call(["yum", "install", "mysql-server", "-y"])
