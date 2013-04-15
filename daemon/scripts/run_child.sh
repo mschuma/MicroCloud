218a7e3ed79ca25d0d4c18ee35a9e209efc5598f
Ankita Pawar <ankscircle@gmail.com>
2013-04-15 15:14:04 -0400

    Script Change for SSL

--- daemon/scripts/run_child.sh
+++ daemon/scripts/run_child.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
-java -cp child_config.properties:child_daemon.jar:dependencies/json-simple-1.1.1.jar -Djavax.net.ssl.trustStore=/home/vcluser/dist/mySrvKeystore -Djavax.net.ssl.trustStorePassword=vclroot -Djavax.net.ssl.keyStore=myCliKeystore -Djavax.net.ssl.keyStorePassword=vclroot edu.ncsu.csc.microcloud.daemon.child.ChildDaemon
+java -cp child_config.properties:child_daemon.jar:dependencies/json-simple-1.1.1.jar -Djavax.net.ssl.trustStore=/home/vcluser/dist/mySrvKeystore -Djavax.net.ssl.trustStorePassword=vclroot -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=vclroot edu.ncsu.csc.microcloud.daemon.child.ChildDaemon
--- daemon/scripts/run_parent.sh
+++ daemon/scripts/run_parent.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp parent_config.properties:parent_daemon.jar:dependencies/json-simple-1.1.1.jar:dependencies/mysql-connector-java-5.1.22-bin.jar  -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=vclroot -Djavax.net.ssl.trustStore=myCliKeystore -Djavax.net.ssl.trustStorePassword=vclroot edu.ncsu.csc.microcloud.daemon.parent.ParentDaemon
+java -cp parent_config.properties:parent_daemon.jar:dependencies/json-simple-1.1.1.jar:dependencies/mysql-connector-java-5.1.22-bin.jar -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=vclroot -Djavax.net.ssl.trustStore=mySrvKeystore -Djavax.net.ssl.trustStorePassword=vclroot edu.ncsu.csc.microcloud.daemon.parent.ParentDaemon
